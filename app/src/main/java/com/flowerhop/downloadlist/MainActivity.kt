34958f6a734a75495b0878d7cdc8f7f3d3fe5be2
Flowerhop <l941166@hotmail.com>
2021-10-16 22:08:37 +0800

    Add recycler view and combine all of these components in MainActivity.

--- app/src/main/java/com/flowerhop/downloadlist/MainActivity.kt
+++ app/src/main/java/com/flowerhop/downloadlist/MainActivity.kt
@@ -2,10 +2,50 @@ package com.flowerhop.downloadlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.flowerhop.downloadlist.databinding.ActivityMainBinding
+import com.flowerhop.downloadlist.model.repository.FakeRepository
+import com.flowerhop.downloadlist.mvvm.AnyViewModelFactory
+import com.flowerhop.downloadlist.ui.CloudFileListAdapter
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        val recyclerView = binding.cloudFileList
+
+        val viewModelFactory = AnyViewModelFactory {
+            CloudFileListViewModel(FakeRepository())
+        }
+
+        val viewModel = ViewModelProvider(this, viewModelFactory).get(CloudFileListViewModel::class.java)
+        val adapter = CloudFileListAdapter(object : CloudFileListAdapter.OnEventHandler {
+            override fun onClick(position: Int) {
+                viewModel.selectOn(position)
+            }
+
+            override fun onDownload(position: Int) {
+                viewModel.download(position)
+            }
+
+            override fun onCancel(position: Int) {
+                viewModel.cancelDownload(position)
+            }
+        })
+
+        viewModel.cloudFileStates.observe(this) {
+            adapter.submitList(it.toMutableList())
+        }
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.itemAnimator = null
+        viewModel.queryFiles()
     }
 }
