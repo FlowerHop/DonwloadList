2aff20aa791f71e24f48e66d210d5a3de3c4845a
Flowerhop <l941166@hotmail.com>
2021-10-17 01:32:12 +0800

    Extract DownloadTask class.

---
+++ app/src/main/java/com/flowerhop/downloadlist/model/DownloadTask.kt
@@ -0,0 +1,48 @@
+package com.flowerhop.downloadlist.model
+
+import com.flowerhop.downloadlist.model.repository.OnDownloadListener
+import java.util.concurrent.atomic.AtomicBoolean
+
+class DownloadTask private constructor(
+    private val cloudFile: CloudFile,
+    private var onDownloadListener: OnDownloadListener?): Runnable {
+
+    private val isCancelled = AtomicBoolean(false)
+    override fun run() {
+        val totalSize = cloudFile.size
+        var downloaded = 0L
+        while (downloaded < totalSize) {
+            if (isCancelled.get()) {
+                onDownloadListener?.onCancel()
+                onDownloadListener = null
+                return
+            }
+
+            Thread.sleep(SLEEP_INTERVAL_FOR_DOWNLOADING)
+            downloaded += DOWNLOAD_SPEED
+
+            val downloadedPortion:Float = downloaded.div(totalSize.toFloat())
+            onDownloadListener?.onProgress((downloadedPortion*100).toInt())
+        }
+
+        onDownloadListener?.onComplete()
+        onDownloadListener = null
+    }
+
+    fun cancel() {
+        isCancelled.set(true)
+    }
+
+    companion object {
+        /**
+         * download bytes per 100ms
+         */
+        private const val DOWNLOAD_SPEED = 1000L
+
+        private const val SLEEP_INTERVAL_FOR_DOWNLOADING = 100L
+
+        fun create(cloudFile: CloudFile, onDownloadListener: OnDownloadListener? = null): DownloadTask {
+            return DownloadTask(cloudFile, onDownloadListener)
+        }
+    }
+}
--- app/src/main/java/com/flowerhop/downloadlist/model/repository/FakeRepository.kt
+++ app/src/main/java/com/flowerhop/downloadlist/model/repository/FakeRepository.kt
@@ -1,6 +1,7 @@
 package com.flowerhop.downloadlist.model.repository
 
 import com.flowerhop.downloadlist.model.CloudFile
+import com.flowerhop.downloadlist.model.DownloadTask
 import java.util.concurrent.Executor
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -65,49 +66,5 @@ class FakeRepository: FileRepository {
     companion object {
         private const val NUMBER_OF_DOWNLOAD_THREAD = 20
         private const val SLEEP_INTERVAL_FOR_QUERYING = 1000L
-    }
-}
-
-class DownloadTask private constructor(
-    private val cloudFile: CloudFile,
-    private var onDownloadListener: OnDownloadListener?): Runnable {
-
-    private val isCancelled = AtomicBoolean(false)
-    override fun run() {
-        val totalSize = cloudFile.size
-        var downloaded = 0L
-        while (downloaded < totalSize) {
-            if (isCancelled.get()) {
-                onDownloadListener?.onCancel()
-                onDownloadListener = null
-                return
-            }
-
-            Thread.sleep(SLEEP_INTERVAL_FOR_DOWNLOADING)
-            downloaded += DOWNLOAD_SPEED
-
-            val downloadedPortion:Float = downloaded.div(totalSize.toFloat())
-            onDownloadListener?.onProgress((downloadedPortion*100).toInt())
-        }
-
-        onDownloadListener?.onComplete()
-        onDownloadListener = null
-    }
-
-    fun cancel() {
-        isCancelled.set(true)
-    }
-
-    companion object {
-        /**
-         * download bytes per 100ms
-         */
-        private const val DOWNLOAD_SPEED = 1000L
-
-        private const val SLEEP_INTERVAL_FOR_DOWNLOADING = 100L
-
-        fun create(cloudFile: CloudFile, onDownloadListener: OnDownloadListener? = null): DownloadTask {
-            return DownloadTask(cloudFile, onDownloadListener)
-        }
     }
 }
